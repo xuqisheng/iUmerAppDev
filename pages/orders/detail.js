bcc4b87f9f0b035764483a1078c4ce7f02f43b19
Yicheng Ye <ye.yicheng123@gmail.com>
2017-02-09 12:59:15 +0800

    order detail buttons

--- pages/orders/detail.js
+++ pages/orders/detail.js
@@ -11,7 +11,13 @@ Page({
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
     this.setData({
-      orderNo: options.orderNo || 'DD5835963228129030'
+      // orderNo: options.orderNo,
+      // orderNo: 'DD5835963228129030' // 取消中 5
+      // orderNo: 'DD5710593313463820' // 待评价 2
+      // orderNo: 'DD2433910090490640' // 已评价 3
+      // orderNo: 'DD5836271871488530' // 已取消 4
+      orderNo: 'DD5819284514093920' // 进行中 1
+      // orderNo: 'DD5819196003434350' // 待付款 0
     });
     this.loadOrder();
   },
@@ -77,10 +83,24 @@ Page({
                     success: function(res) {
                       if (res.data.code == 1) {
                         var data = res.data.data;
-                        wx.showToast({
-                          title: '订单未在15分钟内支付，已取消',
-                          icon: 'success',
-                          duration: 5000
+                        wx.showModal({
+                          title: '提示',
+                          content: '订单未在15分钟内支付，已取消',
+                          showCancel: false,
+                          success: function() {
+                            wx.redirectTo({
+                              url: 'detail',
+                              success: function(res){
+                                // success
+                              },
+                              fail: function() {
+                                // fail
+                              },
+                              complete: function() {
+                                // complete
+                              }
+                            })
+                          }
                         }); 
                         that.setData({
                           showTimer: false
@@ -106,7 +126,7 @@ Page({
                     complete: function(res) {
                       console.log("orderDetail - loadOrder complete")
                     }
-                });
+                  });
                 } else {
                   that.setData({
                     min: diffMin,
@@ -196,5 +216,228 @@ Page({
     wx.makePhoneCall({
       phoneNumber: phone
     })
+  },
+  cancelOrder: function() {
+    var that = this;
+    wx.showModal({
+      title: '提示',
+      content: '是否取消订单？',
+      success: function(res) {
+        if (res.confirm) {
+          wx.request({
+            url: app.globalData.server_url + 'webService/customer/biz/reserve/cancelOrder', 
+            data: {
+              orderNo: that.data.orderNo,
+              customerId: wx.getStorageSync('id')
+            },
+            method: "POST",
+            dataType: "json",
+            header: {
+              'Content-Type': 'application/json;charset=UTF-8;',
+              'X-Token': wx.getStorageSync('X-TOKEN'),
+              'X-Type': 3
+            },
+            success: function(res) {
+              if (res.data.code == 1) {
+                var data = res.data.data;
+                wx.showModal({
+                  title: '提示',
+                  content: '订单已取消',
+                  showCancel: false,
+                  success: function() {
+                    wx.redirectTo({
+                      url: 'detail',
+                      success: function(res){
+                        // success
+                      },
+                      fail: function() {
+                        // fail
+                      },
+                      complete: function() {
+                        // complete
+                      }
+                    })
+                  }
+                }); 
+              } else if (res.data.code == -4) {
+                wx.navigateTo({
+                  url: '../login/login',
+                  success: function(res){
+                    // success
+                  },
+                  fail: function() {
+                    // fail
+                  },
+                  complete: function() {
+                    // complete
+                  }
+                })
+              } else if (res.data.code == 0){
+                wx.showToast({
+                  title: res.data.desc,
+                  icon: 'success',
+                  duration: 5000
+                }); 
+              }
+            },
+            fail: function(res) {
+              console.log("orderDetail - loadOrder fail")
+            },
+            complete: function(res) {
+              console.log("orderDetail - loadOrder complete")
+            }
+        });
+        }
+      }
+    });
+  },
+  applyCancelOrder: function() {
+    var that = this;
+    wx.showModal({
+      title: '提示',
+      content: '是否申请取消订单？',
+      success: function(res) {
+        if (res.confirm) {
+          wx.request({
+            url: app.globalData.server_url + 'webService/customer/biz/reserve/applyCancelOrder', 
+            data: {
+              orderNo: that.data.orderNo,
+              customerId: wx.getStorageSync('id')
+            },
+            method: "POST",
+            dataType: "json",
+            header: {
+              'Content-Type': 'application/json;charset=UTF-8;',
+              'X-Token': wx.getStorageSync('X-TOKEN'),
+              'X-Type': 3
+            },
+            success: function(res) {
+              if (res.data.code == 1) {
+                var data = res.data.data;
+                wx.showModal({
+                  title: '提示',
+                  content: '已申请取消订单',
+                  showCancel: false,
+                  success: function() {
+                    wx.redirectTo({
+                      url: 'detail',
+                      success: function(res){
+                        // success
+                      },
+                      fail: function() {
+                        // fail
+                      },
+                      complete: function() {
+                        // complete
+                      }
+                    })
+                  }
+                });  
+              } else if (res.data.code == -4) {
+                wx.navigateTo({
+                  url: '../login/login',
+                  success: function(res){
+                    // success
+                  },
+                  fail: function() {
+                    // fail
+                  },
+                  complete: function() {
+                    // complete
+                  }
+                })
+              } else if (res.data.code == 0){
+                wx.showToast({
+                  title: res.data.desc,
+                  icon: 'success',
+                  duration: 5000
+                }); 
+              }
+            },
+            fail: function(res) {
+              console.log("orderDetail - loadOrder fail")
+            },
+            complete: function(res) {
+              console.log("orderDetail - loadOrder complete")
+            }
+          });
+        }
+      }
+    })
+  },
+  orderDone: function() {
+    var that = this;
+    wx.showModal({
+      title: '提示',
+      content: '订单是否已完成？',
+      success: function(res) {
+        if (res.confirm) {
+          wx.request({
+            url: app.globalData.server_url + 'webService/customer/biz/order/confirmFinishOrder', 
+            data: {
+              orderNo: that.data.orderNo,
+              customerId: wx.getStorageSync('id'),
+              personnelId: that.data.orderInfo.personnelId
+            },
+            method: "POST",
+            dataType: "json",
+            header: {
+              'Content-Type': 'application/json;charset=UTF-8;',
+              'X-Token': wx.getStorageSync('X-TOKEN'),
+              'X-Type': 3
+            },
+            success: function(res) {
+              if (res.data.code == 1) {
+                var data = res.data.data;
+                wx.showModal({
+                  title: '提示',
+                  content: '订单已确认完成',
+                  showCancel: false,
+                  success: function() {
+                    wx.redirectTo({
+                      url: 'detail',
+                      success: function(res){
+                        // success
+                      },
+                      fail: function() {
+                        // fail
+                      },
+                      complete: function() {
+                        // complete
+                      }
+                    })
+                  }
+                });
+              } else if (res.data.code == -4) {
+                wx.navigateTo({
+                  url: '../login/login',
+                  success: function(res){
+                    // success
+                  },
+                  fail: function() {
+                    // fail
+                  },
+                  complete: function() {
+                    // complete
+                  }
+                })
+              } else if (res.data.code == 0){
+                wx.showToast({
+                  title: res.data.desc,
+                  icon: 'success',
+                  duration: 5000
+                }); 
+              }
+            },
+            fail: function(res) {
+              console.log("orderDetail - loadOrder fail")
+            },
+            complete: function(res) {
+              console.log("orderDetail - loadOrder complete")
+            }
+          });   
+        }
+      }
+    });
   }
 })
